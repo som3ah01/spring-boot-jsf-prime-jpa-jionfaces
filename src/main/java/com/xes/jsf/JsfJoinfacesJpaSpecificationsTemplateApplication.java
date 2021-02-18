2ac8e6649f1a73b61178d818ec1f492196cb1c33
ishebl@cegedim.com <Ismail.SHEBL@cegedim.com>
2021-02-18 13:06:59 +0200

    init files

---
+++ src/main/java/com/xes/jsf/JsfJoinfacesJpaSpecificationsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.xes.jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfJoinfacesJpaSpecificationsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfJoinfacesJpaSpecificationsTemplateApplication.class, args);
+	}
+
+}
